0a29dbc42f0aa2200461a00d82986d4f5c9bb1f1
Johan Bester <johan.bester@yahoo.com>
2020-09-24 15:11:37 -0500

    Ad loops challenge

--- challenges/loops.js
+++ challenges/loops.js
@@ -61,7 +61,8 @@ function readLine() {
 function main() {
     const n = parseInt(readLine(), 10);
 
-    for (let i = 0; i <= 10; i++) {
-        console.log(`${n} x ${i} = `, n*i);
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${n} x ${i} =`, n*i);
     };
 };
+
