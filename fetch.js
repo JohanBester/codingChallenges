a58e6068cdbad7da3b7efc08bdeed6b2946bee26
Johan Bester <johan.bester@yahoo.com>
2020-10-29 12:37:29 -0500

    add new code

---
+++ fetch.js
@@ -0,0 +1,18 @@
+function fetch() {
+  fs = require("fs");
+  let lastNamesList = [];
+  let nameFile = "lastNames.txt";
+  // Reading last name data
+  fs.readFile(nameFile, "utf8", function(err, data) {
+    if (err) throw err;
+    let lstNms = data;
+    lstNms = lstNms.toString().split("\r\n");
+    lastNamesList = lstNms;
+    console.log("typeof lastNamesList is ...", typeof lastNamesList);
+    console.log(lastNamesList);
+  });
+
+  console.log("lastNamesList = ", lastNamesList);
+}
+
+console.log(fetch());
